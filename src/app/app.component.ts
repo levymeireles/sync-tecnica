8a96c2437fdb6a58ea4ca4cd347346dd25473fac
Levy Meireles Rodrigues dos Santos <levymeireles518@gmail.com>
2024-09-11 23:51:48 -0300

    feat: criando component de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import {
-  PoMenuItem,
-  PoMenuModule,
-  PoPageModule,
-  PoToolbarModule,
-} from '@po-ui/ng-components';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    PoToolbarModule,
-    PoMenuModule,
-    PoPageModule
-  ],
+  imports: [NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) },
-  ];
-
-  private onClick() {
-    alert('Clicked in menu item');
-  }
-}
+export class AppComponent {}
---
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-nav-menu',
+  standalone: true,
+  imports: [RouterOutlet, PoToolbarModule, PoMenuModule, PoPageModule],
+
+  templateUrl: './nav-menu.component.html',
+  styleUrl: './nav-menu.component.scss',
+})
+export class NavMenuComponent {
+  public readonly logoTotvs: string = 'assets/icon-totvs.svg';
+
+  public readonly title: string = 'Sync Técnica';
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
+}
